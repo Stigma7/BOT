c33accc9b2ab566dc75d390ee2f7ee366810633a
dimab <dinnao.bi@gmail.com>
2023-12-15 16:28:33 +0200

    Додали залежності, дані бази, створили репозиторій та об'єкт Place

---
+++ src/main/java/com/example/botv2/repository/PlaceRepo.java
@@ -0,0 +1,8 @@
+package com.example.botv2.repository;
+
+import com.example.botv2.model.Place;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlaceRepo extends CrudRepository<Place, Long> {
+
+}
