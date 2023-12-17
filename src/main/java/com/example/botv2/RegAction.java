452c11ac5c00629b7c2f55bb1c71f789715e275a
dimab <dinnao.bi@gmail.com>
2023-12-17 23:55:18 +0200

    trable whis save

--- src/main/java/com/example/botv2/RegAction.java
+++ src/main/java/com/example/botv2/RegAction.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Component
 public class RegAction implements Action {
     @Autowired
-    private PlaceRepo placeRepo;
+    private  PlaceRepo placeRepo;
     private enum RegistrationStage {
         LOCATION, TOWN, ADDRESS, OBLAST,COMPLETED
     }
@@ -64,17 +64,21 @@ public class RegAction implements Action {
                 message.setText(text);
                 message.setReplyMarkup(replyKeyboardMarkup);
 
-                return message;
-
+                break;
             default:
-                text = "Реєстрація завершена. Дякую!";
+                text = "Дякую за увагу";
                 resetState(); // Скидання стану на початковий
                 break;
         }
 
         return new SendMessage(chatId, text);
     }
-
+    private long chatId;
+    private String placeName;
+    private String town;
+    private String oblast;
+    private String address;
+    private String name;
     @Override
     public BotApiMethod callback(Update update) {
         var msg = update.getMessage();
@@ -84,26 +88,34 @@ public class RegAction implements Action {
 
         switch (currentStage) {
             case LOCATION:
-
+                placeName = userInput;
+                System.out.println("Конструктор:"+"LOCATION: "+placeName);
                 currentStage = RegistrationStage.TOWN;
                 text = "Ви обрали назву місця: " + userInput + "\n Натисніть на /new для продовження додавання";
                 break;
             case TOWN:
-                // Збереження номеру телефону (userInput) в базі даних або деінде
+                town = userInput;
                 currentStage = RegistrationStage.ADDRESS;
                 text = "місто успішно додано: " + userInput + "\n Натисніть на /new для продовження додавання";
                 break;
             case ADDRESS:
-                // Збереження адреси (userInput) в базі даних або деінде
+                address = userInput;
                 currentStage = RegistrationStage.OBLAST;
                 text = "Адреса успішно додано: " + userInput + "\n Залишилось зовсім трішечки, натисніть на /new для продовження ";
                 break;
             case OBLAST:
-                // Збереження області (userInput) в базі даних або деінде
+                oblast = userInput;
                 currentStage = RegistrationStage.COMPLETED;
                 text = "Область успішно додано: " + userInput + "\n Ще трохи, натисніть на /new для ЗАВЕРШЕННЯ реєстрації";
+                name = update.getMessage().getChat().getFirstName();
+
+                System.out.println("Конструктор:"+"LOCATION: "+placeName+" " +
+                        "TOWN: "+town+" OBLAST: "+oblast+" ADDRESS: "+address+" USERNAME: "+name);
+                Place newPlace = new Place(placeName, town, oblast,address,name);
+                placeRepo.save(newPlace);
                 break;
             default:
+
                 text = "Реєстрація завершена. Дякую!";
                 resetState(); // Скидання стану на початковий
                 break;
@@ -112,6 +124,6 @@ public class RegAction implements Action {
         return new SendMessage(chatId, text);
     }
     private void resetState() {
-        currentStage = RegistrationStage.EMAIL;
+        currentStage = RegistrationStage.LOCATION;
     }
 }
